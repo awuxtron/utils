396534b29241e1db11711652697f964ec44ac6d4
Awuxtron <awuxtron@gmail.com>
2023-05-21 10:46:42 +0700

    feat: add guards

---
+++ src/guards.ts
@@ -0,0 +1,26 @@
+/**
+ * Type guard to filter out null value.
+ *
+ * @category Guards
+ */
+export function notNull<T>(v: T): v is Exclude<T, null> {
+    return v !== null
+}
+
+/**
+ * Type guard to filter out undefined value.
+ *
+ * @category Guards
+ */
+export function notUndefined<T>(v: T): v is Exclude<T, undefined> {
+    return v !== undefined
+}
+
+/**
+ * Type guard to filter out null-ish value.
+ *
+ * @category Guards
+ */
+export function notNullish<T>(v: T): v is NonNullable<T> {
+    return notNull(v) && notUndefined(v)
+}
