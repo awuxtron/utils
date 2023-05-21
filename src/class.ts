5d755671e2f9d9ec41f4e025eade3ff91ebdc7af
Awuxtron <awuxtron@gmail.com>
2023-05-21 10:24:25 +0700

    feat: add utilities for classes

--- src/class.ts
+++ src/class.ts
@@ -1,4 +1,6 @@
 import { Fn } from './function'
 
 export type Constructor<T = void> = new (...args: any[]) => T
+
 export type MethodArgs<C, M extends keyof C> = C[M] extends Fn ? Parameters<C[M]> : unknown
+export type MethodReturnType<C, M extends keyof C> = C[M] extends Fn ? ReturnType<C[M]> : unknown
