c25ab79cbd244bc3b868eabef562f943743be38e
Awuxtron <awuxtron@gmail.com>
2023-06-08 22:11:36 +0700

    feat: deepmerge

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,2 +1,2 @@
-export { default as deepMerge, all as deepMergeAll, Options as DeepMergeOptions } from 'deepmerge'
+export { default as deepMerge, all as deepMergeAll } from 'deepmerge'
 export { debounce, throttle } from 'throttle-debounce'
